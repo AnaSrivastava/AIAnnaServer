520300a56f684c3c0c8042b3afac4bab94b0e352
AS <ananyavsrivastava@gmail.com>
2019-06-27 14:51:28 +0530

    Added SSL

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const db=knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-tapered-58839',
-      user : 'postgres',
-      password : 'psql',
-      database : 'aianna'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
